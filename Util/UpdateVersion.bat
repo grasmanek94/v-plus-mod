49b9105f7e553fd0ba1aced3a63a71e1b2bd6154
Gamer_Z <grasmanek94@gmail.com>
2016-04-17 18:27:40 +0200

    fixed updating version without temp file

--- Util/UpdateVersion.bat
+++ Util/UpdateVersion.bat
@@ -1,17 +1,10 @@
-@echo off
-
+@ECHO OFF
 cd /d %~dp0
 cd ../Common/Common.Version
 copy /b Version.cxx +,,
 echo touched version.cxx ...
-
-git rev-list HEAD --count | (set /p _COUNT= & set _COUNT)
-echo %_COUNT% > auto_version.h
-
-FOR /f "tokens=1" %%a in ('findstr /R "^[0-9][0-9]*" auto_version.h') do set _BUILD_VERSION=%%a
-
-echo #define BUILD_VERSION %_BUILD_VERSION% > auto_version.h
-echo Updated version to %_BUILD_VERSION%
-
+for /f "delims=" %%a in ('git rev-list HEAD --count') do set _COUNT=%%a
+echo #define BUILD_VERSION %_COUNT% > auto_version.h
+echo Updated version to %_COUNT%
 cd /d %~dp0
 REM exit 0
