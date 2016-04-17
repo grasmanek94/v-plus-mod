1df3198ce32241f3f2d111ac232b1cf930f79c88
Gamer_Z <grasmanek94@gmail.com>
2016-04-17 18:04:10 +0200

    update version update

--- Util/UpdateVersion.bat
+++ Util/UpdateVersion.bat
@@ -1,12 +1,14 @@
-@echo off
-cd /d %~dp0
+@ECHO OFF
+cd /d "%~dp0"
 cd ../Common/Common.Version
 copy /b Version.cxx +,,
 echo touched version.cxx ...
 
-git rev-list HEAD --count > auto_version.h
-FOR /f "tokens=1" %%a in ('findstr /R "^[0-9][0-9]*" auto_version.h') do set _BUILD_VERSION=%%a
+cmd /c git rev-list HEAD --count > temp_auto_version.h
+
+FOR /f "tokens=1" %%a in ('findstr /R "^[0-9][0-9]*" temp_auto_version.h') do set _BUILD_VERSION=%%a
 
 echo #define BUILD_VERSION %_BUILD_VERSION% > auto_version.h
 echo Updated version to %_BUILD_VERSION%
+cd /d "%~dp0"
 REM exit 0
